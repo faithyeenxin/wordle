57683e00d9501d1f6dd3e2d690b32da55a070c6c
Faith Ye <faithye@Faiths-MacBook-Air.local>
2022-08-05 15:27:27 +0800

    added modal

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 import $ from "jquery";
 
 console.log($);
+
+const $modal = $(".modal");
+const $startBtn = $(".modal_button");
+
+window.onload = (event) => {
+  setTimeout(() => {
+    $modal.addClass("modal_visible");
+  }, 1000);
+};
+
+$startBtn.on("click", () => {
+  $modal.removeClass("modal_visible");
+});
